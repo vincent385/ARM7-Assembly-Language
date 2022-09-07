5c3c21f6e1a17befff843fefb62b45e6965b79f3
vincent-bell <vincentbell103@outlook.com>
2022-09-07 10:38:26 +0100

    refactored special_addvar.asm

--- thumb scripts/special_addvar.asm
+++ thumb scripts/special_addvar.asm
@@ -4,25 +4,27 @@
 .thumb_func
 
 main:
+    # push the registers to be used and link register onto stack
     push {r0-r1, lr}
 
-    @ load pointer addresses into registers
+    # load pointer addresses into registers
     ldr r0, =(0x20370B8) @ 0x8000
     ldr r1, =(0x20370D0) @ 0x800D
 
-    @ load half words (2 bytes) from the data pointed to by the addresses we loaded
+    # load half words (2 bytes) from the data pointed to by the addresses we loaded
     ldrh r0, [r0]
     ldrh r1, [r1]
 
     # addvar 0x8000 0x800D
     add r0, r0, r1
 
-    # load the address of 0x800D again because we lost it after line 15
+    # load the address of 0x800D again because we lost it after line 16
     ldr r1, =(0x20370D0)
 
     # store the value in r0 at the address stored in r1
     strh r0, [r1]
 
+    # pop the registers used and program counter
     pop {r0-r1, pc}
 
 .align 2
